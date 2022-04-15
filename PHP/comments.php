32c1365005a2690bee567124f90d050a4fae54fb
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-15 11:43:00 +1000

    comments single line and miltiple line comments

--- PHP/comments.php
+++ PHP/comments.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
+// single line comments are
+// This is single line comments
+# this is also a single line comments
+
+#Multiple line comments
+/*
+ * This is multiple line comments
+ */
+//we can also use comment to leve out parts of the code
+$x = 5 + /* + 15 */ + 5;
+echo "The sum of the variable is " . $x . "<br>" ;
+?>
+</body>
+</html>
