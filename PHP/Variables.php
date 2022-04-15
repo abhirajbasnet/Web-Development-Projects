b191c5e88ad3e600f9bd09367b842b9f281e7be2
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-15 12:10:38 +1000

    variables

--- PHP/Variables.php
+++ PHP/Variables.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
+//Variables are the container to store the information
+//creating (Declaring) the php Variables
+//in php variables starts with the $ sign, followed by the name of the variable
+// when we assign the text variable we have to put quotes around the value
+//Variable can have short name like (a and b) or can have the descriptive name as carName ot total-value
+// Variables name must start with letter or the underscore character like $txt or $_what
+//Variables name cannot be start with numbers
+// Variables name can only contain the alpha-numeric character and underscore (a-z, 0-9, and _)
+// Variables are case-sensitive $age and $Age are two different variables
+
+
+$txt = "Hello World!";
+$x = 5;
+$y = 10.5;
+$text = "Hello there";
+echo $text . "<br>". $txt ."<br>". " sum of variables x and y is ". ($x + $y);
+?>
+</body>
+</html>
+
