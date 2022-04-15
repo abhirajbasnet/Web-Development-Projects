97c4533f183c18a8e77eaf840d3846d9ade08eec
abhirajbasnet11@gmail.com <abhirajbasnet11@gmail.com>
2022-04-15 11:35:57 +1000

    studied the syntax

--- PHP/Syntax.php
+++ PHP/Syntax.php
@@ -1,1 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<h>My First php page <br></h>
 <?php
+//php script is executed in the server, and plain html result is sent back to the browser
+//basic php tag is
+//
+// //php codes goes here. The default file extension name is .php
+//echo is used to output the text
+// php is not case-sensitive which means in keywords (if else while echo etc) class function
+// and user-defined functions are not case-sensitive
+// However all variable name are case-sensitive
+echo "Hello World <br>";
+//for instances
+ecHo "Hello World <br>";
+EcHo "Hellop World <br>";
+ECHO "Hello World <br>";
+
+$color = "red";
+echo "My car is " .$color . "<br>";
+echo "My phone is " .$color . " in color". "<br>";
+?>
+</body>
+</html>
+
+
+
+
+
